a1f8df6384243f460d45698c721f860619a9f9c5
Raymond Toy <rtoy@google.com>
2013-05-31 11:10:30 -0700

    Safari doesn't seem to have performance.now(), so don't use it on Safari.

--- more/simpl/webrtc-webaudio/js/main.js
+++ more/simpl/webrtc-webaudio/js/main.js
@@ -15,7 +15,9 @@ function trace(text) {
   if (text[text.length - 1] == '\n') {
     text = text.substring(0, text.length - 1);
   }
-  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+  if (typeof performance === "object") {
+    console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+  }
 }
 
 function call() {
