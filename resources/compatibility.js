b3e1fba65ca3467a57e7f7654fe364435e6bda9e
Raymond Toy <rtoy@chromium.org>
2015-02-03 14:26:57 -0800

    Check for AudioContext first so Chrome doesn't give deprecation warnings about webkitAudioContext.

--- resources/compatibility.js
+++ resources/compatibility.js
@@ -1,11 +1,13 @@
 // Support for both prefixed and unprefixed AudioContext and OfflineAudioContext.
 // We don't support other legacy names.
-if (!((typeof webkitAudioContext === "function")
-      || (typeof AudioContext === "function")
+if (!((typeof AudioContext === "function")
+      || (typeof AudioContext === "object")
+      || (typeof webkitAudioContext === "function")
       || (typeof webkitAudioContext === "object")
-      || (typeof AudioContext === "object"))) {
+      )) {
   alert("Sorry! Web Audio not supported by this browser");
 }
+
 if (window.hasOwnProperty('webkitAudioContext') &&
     !window.hasOwnProperty('AudioContext')) {
     window.AudioContext = webkitAudioContext;
