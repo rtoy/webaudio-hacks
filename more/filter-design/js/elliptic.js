b561f1f8b0618850ff75186faa305830135a632f
Raymond Toy <toy.raymond@gmail.com>
2016-06-09 16:15:38 -0700

    Implement more Jacobi elliptic functions.
    Needs testing.

--- more/filter-design/js/elliptic.js
+++ more/filter-design/js/elliptic.js
@@ -13,6 +13,16 @@ function jacobi_sn(u, m) {
     return elliptic_sn_descending(u, m);
 }
 
+function complex_jacobi_sn(u, m) {
+    if (m == 0) {
+        return complex_sin(u);
+    }
+    if (m == 1) {
+        return complex_tanh(u);
+    }
+    return complex_elliptic_sn_descending(u, m);
+}
+
 function jacobi_snk(u, k) {
     return jacobi_sn(u, k * k);
 }
@@ -31,12 +41,40 @@ function elliptic_sn_descending(u, m) {
     return (1 + root_mu) * new_sn / (1 + root_mu * new_sn * new_sn);
 }
 
+function complex_elliptic_sn_descending(u, m) {
+    if (m == 1) {
+        // sn(u, 1) = tanh(u)
+        return complex_tanh(u);
+    }
+    if (Math.abs(m) <= Number.EPSILON * Math.abs(u)) {
+        return complex_sin(u);
+    }
+
+    var {v, mu, root_mu} = complex_descending_transform(u, m);
+    var new_sn = complex_elliptic_sn_descending(v, mu);
+    return cdiv(cmul(rcadd(1, root_mu), new_sn),
+                rcadd(1, cmul(root_mu, cmul(new_sn, new_sn))));
+}
+
 // Descending Landen transform
 function descending_transform(u, m) {
     var root_m1 = Math.sqrt(1 - m);
     var root_mu = (1 - root_m1) / (1 + root_m1);
     var mu = root_mu * root_mu;
     var v = u / (1 + root_mu);
+
+    return {
+        v: v,
+        mu: mu,
+        root_mu: root_mu
+    };
+}
+
+function complex_descending_transform(u, m) {
+    var root_m1 = csqrt(rcsub(1, m));
+    var root_mu = cdiv(rcsub(1, root_m1), rcadd(1, root_m1));
+    var mu = cmul(root_mu, root_mu);
+    var v = cdiv(u, rcadd(1, root_mu));
 
     return {
         v: v,
@@ -57,6 +95,19 @@ function jacobi_cn(u, m) {
     var {v, mu, root_mu1} = ascending_transform(u, m);
     var d = jacobi_dn(v, mu);
     return (1 + root_mu1) / mu * ((d * d - root_mu1) / d);
+}
+
+function complex_jacobi_cn(u, m) {
+    if (m == 0) {
+        return complex_cos(u);
+    }
+    if (m == 1) {
+        return rcdiv(1, complex_cosh(u));
+    }
+    var {v, mu, root_mu1} = complex_ascending_transform(u, m);
+    var d = complex_jacobi_dn(v, mu);
+    return cmul(cdiv(rcadd(1, root_mu1), mu),
+                cdiv(csub(cmul(d, d), root_mu1), d));
 }
 
 function jacobi_cnk(u, k) {
@@ -157,6 +208,42 @@ function carlson_rf(x, y, z) {
     return s / Math.sqrt(an);
 }
 
+function complex_carlson_rf(x, y, z) {
+    var xn = x;
+    var yn = y;
+    var zn = z;
+    var a = cdivr(cadd(cadd(xn, yn), zn), 3);
+    var eps = Math.max(cabs(csub(a, xn)), cabs(csub(a, yn)), cabs(csub(a, zn))) / errtol(x, y, z);
+    var an = a;
+    var power4 = 1;
+    var n = 0;
+
+    while (power4 * eps > cabs(an)) {
+        var xnroot = csqrt(xn);
+        var ynroot = csqrt(yn);
+        var znroot = csqrt(zn);
+        var lam = cmul(xnroot, ynroot);
+        lam = cadd(lam, cmul(xnroot, znroot));
+        lam = cadd(lam, cmul(ynroot, znroot));
+        power4 *= 1/4;
+        xn = cdivr(cadd(xn, lam), 4);
+        yn = cdivr(cadd(yn, lam), 4);
+        zn = cdivr(cadd(zn, lam), 4);
+        an = cdivr(cadd(an, lam), 4);
+        ++n;
+    }
+    var xndev = cdiv(rcmul(power4, csub(a, x)), an);
+    var yndev = cdiv(rcmul(power4, csub(a, y)), an);
+    var zndev = rcmul(-1, cadd(xndev, yndev));
+    var ee2 = csub(cmul(xndev, yndev), rcmul(6, cmul(zndev, zndev)));
+    var ee3 = cmul(cmul(xndev, yndev), zndev);
+    //var s = 1 - ee2/10 + ee3/14 + ee2*ee2/24 - 3/44*ee2*ee3;
+    var s = cadd(cdivr(cmul(ee2, ee2), 24), rcmul(-3/44, cmul(ee2, ee3)));
+    s = cadd(s, cadd(cdivr(ee2, 10), cdivr(ee3, 14)));
+    s = cadd(s, {re: 1, im: 0});
+    return cdiv(s, csqrt(an));
+}
+
 function inverse_jacobi_sn(u, m) {
     return u * carlson_rf(1 - u*u, 1 - m*u*u, 1);
 }
@@ -191,9 +278,15 @@ function inverse_jacobi_cdk(w, k) {
     return inverse_jacobi_cd(w, k*k);
 }
 
-function inverse_jacobi_sni(w, m) {
-    // u = inverse_jacobi_sn(i*w,m)
-    // u = i*inverse_jacobi_sc(w,m)
-    // inverse_jacobi_sc(w,m) = inverse_jacobi_sn(w/sqrt(1+w^2),m)
-    return {im: inverse_jacobi_sn(w/Math.sqrt(1+w*w),m)}
+function complex_inverse_jacobi_sn(u, m) {
+    var arg1 = cmul(u,u);
+    arg1 = rcsub(1, arg1);
+
+    var arg2 = rcsub(1, rcmul(m, cmul(u,u)));
+    console.log("arg1");
+    console.log(arg1)
+    console.log("arg2");
+    console.log(arg2)
+        
+    return cmul(u, complex_carlson_rf(arg1, arg2,{re: 1, im:0}));
 }
